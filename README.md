7826f333905154785003a87484add7b72cf6ed6d
stanton <mike@Mikes-MacBook-Pro.local>
2020-08-19 11:57:32 -0700

    scripts

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # osx
 Useful scripts and setup configurations.
+
+Add alias for git locations
+Edit /proc alias's for mac os
+
+git clone https://github.com/Redw0od/osx.git ~/git/osx
+sh ~/git/osx/setup_basic.sh
+
+
+Setup Slack Workspaces
+respond-software (respond)
+teamoperaevent (oe)
+gruntwork-community (respond)
+devopsengineers (personal)
+w3-23-18 (personal)
+w3-23-20 (personal)
+
+
--- setup_basic.sh
+++ setup_basic.sh
@@ -1,99 +1,114 @@
-echo "\nInstalling Homebrew"
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
-
-echo "\nInstalling wget"
-brew install wget
-echo "\nInstalling git & extras"
-brew install git
-brew install git-extras
-
-echo "\nInstalling bash"
-brew install bash
-echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
-chsh -s /usr/local/bin/bash
-brew install bash-git-prompt
-
-## add code to bashrc
+echo "\nCloning Anbile Homebrew Installer"
+git clone https://github.com/Redw0od/ansible-role-homebrew.git ~/git/osx/roles/ansible-role-homebrew
 
 
 
-echo "\nInstalling firefox"
-brew cask install firefox
+# echo "\nInstalling Homebrew"
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
-##broke
-echo "\nInstalling VS Code"
-brew cask install visual-studio-code
-
-echo "\nInstalling htop"
-brew install htop
-
-echo "\nInstalling jq"
-brew install jq
-
-echo "\nInstalling tldr"
-brew install tldr
-
-echo "\nInstalling ncdu"
-brew install ncdu
-
-echo "\nInstalling python3"
-brew install python3
-
-echo "\nInstalling coreutils"
-brew install coreutils
-
-echo "\nInstalling openssl"
-brew install openssl
-
-echo "\nInstalling awscli"
-brew install awscli
-
-echo "\nInstalling go"
-brew install go
-
-echo "\nInstalling nmap"
-brew install nmap
-
-echo "\nInstalling node"
-brew install node
-
-echo "\nInstalling nvm"
-brew install nvm
-
-#add bashrc stuff
+# echo "\nInstalling ansible"
+# brew install ansible
 
 
-echo "\nInstalling ansible"
-brew install ansible
 
-echo "\nInstalling terraform"
-brew install terraform
 
-echo "\nInstalling terragrunt"
-brew install terragrunt
+# echo "\nCalling Ansible Playbook: setup-common"
+# ansible-playbook ansible-setup-common.yml
 
-echo "\nInstalling 7zip"
-brew install p7zip
+# echo "\nInstalling wget"
+# brew install wget
+# echo "\nInstalling git & extras"
+# brew install git
+# brew install git-extras
 
-echo "\nInstalling k8s"
-brew install kubernetes-cli
+# echo "\nInstalling github desktop"
+# brew cask install github
 
-echo "\nInstalling gpg"
-brew install gnupg
+# echo "\nInstalling bash"
+# brew install bash
+# echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
+# chsh -s /usr/local/bin/bash
+# brew install bash-git-prompt
 
-echo "\nInstalling wireshark"
-brew install wireshark
+# ## add code to bashrc
 
-echo "\nInstalling openssh"
-brew install openssh
 
-echo "\nInstalling rsync"
-brew install rsync
 
-echo "\nInstalling OpenVPN-Connect"
-brew cask install openvpn-connect
+# echo "\nInstalling firefox"
+# brew cask install firefox
 
-echo "\nInstalling Lastpass"
-brew cask install lastpass
+# ##broke
+# echo "\nInstalling VS Code"
+# brew cask install visual-studio-code
+
+# echo "\nInstalling htop"
+# brew install htop
+
+# echo "\nInstalling jq"
+# brew install jq
+
+# echo "\nInstalling tldr"
+# brew install tldr
+
+# echo "\nInstalling ncdu"
+# brew install ncdu
+
+# echo "\nInstalling python3"
+# brew install python3
+
+# echo "\nInstalling coreutils"
+# brew install coreutils
+
+# echo "\nInstalling openssl"
+# brew install openssl
+
+# echo "\nInstalling awscli"
+# brew install awscli
+
+# echo "\nInstalling go"
+# brew install go
+
+# echo "\nInstalling nmap"
+# brew install nmap
+
+# echo "\nInstalling node"
+# brew install node
+
+# echo "\nInstalling nvm"
+# brew install nvm
+
+# #add bashrc stuff
+
+
+
+# echo "\nInstalling terraform"
+# brew install terraform
+
+# echo "\nInstalling terragrunt"
+# brew install terragrunt
+
+# echo "\nInstalling 7zip"
+# brew install p7zip
+
+# echo "\nInstalling k8s"
+# brew install kubernetes-cli
+
+# echo "\nInstalling gpg"
+# brew install gnupg
+
+# echo "\nInstalling wireshark"
+# brew install wireshark
+
+# echo "\nInstalling openssh"
+# brew install openssh
+
+# echo "\nInstalling rsync"
+# brew install rsync
+
+# echo "\nInstalling OpenVPN-Connect"
+# brew cask install openvpn-connect
+
+# echo "\nInstalling Lastpass"
+# brew cask install lastpass
 
